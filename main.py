e8c1d4c41e23d90f0e6bcfe5fb7b8849709c7291
Benjamin Molnar <benjamin.molnar@student.tuke.sk>
2024-11-23 13:51:21 +0100

    commit

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import requests
 from openai import OpenAI
-#do zatvoriek pridat api kluc
 client = OpenAI()
 
 response = client.images.generate(
     model="dall-e-3",
-    prompt="a white siamese cat",
+    prompt="Porsche gt3 rs",
     size="1024x1024",
     quality="standard",
     n=1,
 )
 
 image_url = response.data[0].url
+
+image_data = requests.get(image_url).content
+
+with open("generated_image.png", "wb") as f:
+    f.write(image_data)
+
+print("Obrázok bol uložený ako 'generated_image.png'")
